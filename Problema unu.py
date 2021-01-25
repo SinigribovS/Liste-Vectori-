a2f9121127e2cea3d3dabed3f3d780de62efb2a7
SinigribovS <71001180+SinigribovS@users.noreply.github.com>
2021-01-25 19:11:06 +0200

    Add files via upload

---
+++ Problema unu.py
@@ -0,0 +1,20 @@
+#Continutul listei
+lista1=list(eval(input('Dati lista de numere:')))
+print(lista1)
+#Lista in ordine crescatoare 
+lista2=sorted(lista1)
+print(lista2)
+#Lista in ordine descrescatoare 
+lista3=sorted(lista1,reverse=True)
+print(lista3)
+#Numarul de elemente 
+print(len(lista1))
+#Elementul maxim si minim
+print(max(lista1),min(lista1),sep="\n")
+#La coada sa adaugat "- 111"
+lista4=lista1+[111]
+print(lista4)
+#Pe pozitia a doua se adauga elementul "- 222"
+lista5=lista1
+lista5.insert(1, 222)
+print(lista5)
